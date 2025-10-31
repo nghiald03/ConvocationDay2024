2b35ad955c092ca758106fb94eb6ee55609e4226
baothien-se <hoangthiennt2003@gmail.com>
2025-10-31 22:59:39 +0700

    abc

--- mobile/lib/config/router.dart
+++ mobile/lib/config/router.dart
@@ -3,6 +3,7 @@ import 'package:mobile/home.dart';
 import 'package:mobile/src/auth/views/login.dart';
 
 final GoRouter router = GoRouter(
+  initialLocation: '/login',
   routes: [
     GoRoute(path: '/login', builder: (_, __) => const LoginScreen()),
     GoRoute(path: '/home', builder: (_, __) => const HomeScreen()),
