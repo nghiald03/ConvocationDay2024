ddc2f81b31d87506b90e7f721a1a61c96943013b
Lê Đại Nghĩa <92242188+ledainghia@users.noreply.github.com>
2025-11-20 22:39:53 +0700

    feat: update ports and URLs in docker-compose and environment files for backend service

--- fe/app/(protected)/notify/page.tsx
+++ fe/app/(protected)/notify/page.tsx
@@ -212,7 +212,7 @@ export default function NotifyMockPage() {
 
   // ================== SignalR (tự connect + tự join group trong hook) ==================
   const { connection, connectionState, isConnected } = useSignalR({
-    hubUrl: 'http://210.245.25.74:85/chat-hub', // Đổi theo BE của anh
+    hubUrl: 'http://210.245.25.74:88/chat-hub', // Đổi theo BE của anh
     autoConnect: true,
     forceWebsockets: true, // BE đã hỗ trợ WS → tránh negotiate
     stopDelayMs: 3000,
