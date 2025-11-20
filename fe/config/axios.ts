e75a90b60dfef7fb73661a5245408233f616a868
Lê Đại Nghĩa <92242188+ledainghia@users.noreply.github.com>
2025-11-21 00:08:08 +0700

    feat: update API base URL to new server port

--- fe/config/axios.ts
+++ fe/config/axios.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 //   ? `${process.env.NEXT_PUBLIC_SITE_URL}/api`
 //   : 'http://localhost:85/api';
 
-const BASE_URL = 'http://210.245.25.74:85/api';
+const BASE_URL = 'http://210.245.25.74:88/api';
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
